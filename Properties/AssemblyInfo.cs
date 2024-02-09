4f441cf22b8cd553f4396e05c3c1668fc81bc5a9
J3d1x <okulovroman@bk.ru>
2024-02-10 04:06:03 +0500

    third

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3066d87-3d4a-41c5-ae58-3f5cf3934db9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using _3bilet;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Form1 form = new Form1();
+
+            form.textBox1.Text = "12354,42403034600475E+1310 ";
+            form.textBox2.Text = "12354,42403034600475E+1310 ";
+            form.textBox3.Text = "12354,42403034600475E+1310 ";
+            form.textBox4.Text = "12354,42403034600475E+1310 ";
+            form.textBox5.Text = "12354,42403034600475E+1310 ";
+            form.button1_Click(null, null);
+
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Form1 form = new Form1();
+
+            form.textBox1.Text = "-1";
+            form.textBox2.Text = "-5";
+            form.textBox3.Text = "1";
+            form.textBox4.Text = "2";
+            form.textBox5.Text = "3";
+            form.button1_Click(null, null);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Form1 form = new Form1();
+
+            form.textBox1.Text = null;
+            form.textBox2.Text = "1";
+            form.textBox3.Text = "2";
+            form.textBox4.Text = "3";
+            form.textBox5.Text = "3";
+            form.button1_Click(null, null);
+
+        }
+    }
+
+}
